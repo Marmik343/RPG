ad01abd7d93d6d6561f01f397168364bac8272cf
Marmik343 <31364871+Marmik343@users.noreply.github.com>
2020-03-13 09:38:50 +0530

    Add files via upload

---
+++ inventory.py
@@ -0,0 +1,8 @@
+class Item:
+    def __init__(self, name: object, type: object, description: object, prop: object) -> object:
+        self.name = name
+        self.type = type
+        self.description = description
+        self.prop = prop
+
+
